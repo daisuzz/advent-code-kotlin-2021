29b87ffe7ebb23fa2ca5285b69c02e5c7c1a7a44
daisuzz <daisaku.rugby@gmail.com>
2021-12-10 23:18:43 +0900

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var cnt = 0
+        (1 until input.size).forEach {
+            if (input[it].toInt() - input[it - 1].toInt() > 0) cnt++
+        }
+        return cnt
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var cnt = 0
+        (3 until input.size).forEach {
+            if (input[it].toInt() > input[it - 3].toInt()) cnt++
+        }
+        return cnt
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
