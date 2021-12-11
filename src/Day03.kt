66bc5f81b1eced3543ef0ef598d57e16c7ae73b9
daisuzz <daisaku.rugby@gmail.com>
2021-12-11 15:26:31 +0900

    improve Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -20,31 +20,8 @@ fun main() {
 
     fun part2(input: List<CharArray>): Int {
 
-        var oxyGenCandidateList = input
-        var oxyDigit = 0
-
-        while (oxyGenCandidateList.size > 1) {
-
-            val frequency = oxyGenCandidateList.groupingBy { it[oxyDigit] }.eachCount()
-            val mostFrequencyChar = if (frequency.getOrDefault('1', 0) >= frequency.getOrDefault('0', 0)) '1' else '0'
-
-            oxyGenCandidateList = oxyGenCandidateList.filter { it[oxyDigit] == mostFrequencyChar }
-
-            oxyDigit++
-        }
-
-        var co2CandidateList = input
-        var co2Digit = 0
-
-        while (co2CandidateList.size > 1) {
-
-            val frequency = co2CandidateList.groupingBy { it[co2Digit] }.eachCount()
-            val mostFrequencyChar = if (frequency.getOrDefault('1', 0) >= frequency.getOrDefault('0', 0)) '0' else '1'
-
-            co2CandidateList = co2CandidateList.filter { it[co2Digit] == mostFrequencyChar }
-
-            co2Digit++
-        }
+        val oxyGenCandidateList = input.filterByColumn('1', '0')
+        val co2CandidateList = input.filterByColumn('0', '1')
 
         val oxygenGeneratorRating = oxyGenCandidateList.first().joinToString("").toInt(2)
         val co2ScrubberRating = co2CandidateList.first().joinToString("").toInt(2)
@@ -55,4 +32,17 @@ fun main() {
     val input = readInput("Day03").map { it.toCharArray() }
     println(part1(input))
     println(part2(input))
+}
+
+fun List<CharArray>.filterByColumn(desiredCharOnMostFrequency: Char, other: Char): List<CharArray> {
+    var candidateList = this
+    var digit = 0
+    while (candidateList.size > 1) {
+        val frequency = candidateList.groupingBy { it[digit] }.eachCount()
+        val char =
+            if (frequency.getOrDefault('1', 0) >= frequency.getOrDefault('0', 0)) desiredCharOnMostFrequency else other
+        candidateList = candidateList.filter { it[digit] == char }
+        digit++
+    }
+    return candidateList
 }
